f212b25a7600bcd07fa34b022e88b774c736e21a
Ghost <yasinabdurakhmanov@gmail.com>
2021-12-10 20:38:28 +0300

    changes

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
 import os
-import telebot
 from dotenv import load_dotenv
-
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 
 load_dotenv()
 
-token = os.getenv('token')
+api_id = os.getenv('api_id')
+api_hash = os.getenv('api_hash')
+session_str = os.getenv('session_str')
+
+# with TelegramClient(StringSession(), api_id, api_hash) as client:
+#     print("Your session string is:", client.session.save())
+
+client = TelegramClient(session_str, api_id=api_id, api_hash=api_hash)
 
 
-def test_send_message(self):
-    text = 'CI Test Message'
-    tb = telebot.TeleBot(token)
-    ret_msg = tb.send_message(CHAT_ID, text)
-    assert ret_msg.message_id
+async def print_message():
+    message = await client.get_messages('TelethonSnippets', ids=3)
+    print("MESSAGE:", end="\n-------\n")
+    print(message.text)
+
+
+with client:
+    client.loop.run_until_complete(print_message())
